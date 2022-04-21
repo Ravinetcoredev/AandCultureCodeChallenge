567c53a7e7fa953cf8356768e62fedcc32af9ddf
Ravi Kiran Ayinampudi <rxa80430@ucmo.edu>
2022-04-21 07:41:03 -0400

    Get Breweries Restful api data. Deserialize json to current data.

---
+++ Brewery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AandCultureChallenge
+{
+    public class Brewery
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Brewery_Type { get; set; }
+        public string Street { get; set; }
+        public string Address_2 { get; set; }
+        public string Address_3 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string County_Province { get; set; }
+        public string Postal_Code { get; set; }
+        public string Country { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string Phone { get; set; }
+        public string Website_Url { get; set; }
+        public string Updated_At { get; set; }
+        public string Created_At { get; set; }
+    }
+}
---
+++ Controllers/BreweriesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AandCultureChallenge.Controllers
+{
+    [Route("[controller]")]
+    public class BreweriesController : Controller
+    {
+        public async void Index()
+        {
+        }
+
+        [HttpGet]
+        [Route("GetAllBreweries")]
+        public async Task<List<Brewery>> GetAllBreweries()
+        {
+            var response = await GetBreweriesData();
+            List<Brewery> lstBreweries = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Brewery>>(response.Content);
+            return lstBreweries;
+        }
+
+        public async Task<RestResponse> GetBreweriesData()
+        {
+            var client = new RestClient("https://api.openbrewerydb.org/breweries");
+            var response = await client.ExecuteAsync(new RestRequest());
+            return  response;
+        }
+    }
+}
+
+
+
+    
