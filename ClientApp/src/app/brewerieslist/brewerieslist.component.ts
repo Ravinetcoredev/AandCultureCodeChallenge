63816e339a0550a465c883ecb88e61f9ec84712a
Ravi Kiran Ayinampudi <rxa80430@ucmo.edu>
2022-04-21 06:29:31 -0400

    Add brewerieslist component Include in the menu navigation with specified route url

---
+++ ClientApp/src/app/brewerieslist/brewerieslist.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brewerieslist',
+  templateUrl: './brewerieslist.component.html',
+  styleUrls: ['./brewerieslist.component.css']
+})
+export class BrewerieslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
