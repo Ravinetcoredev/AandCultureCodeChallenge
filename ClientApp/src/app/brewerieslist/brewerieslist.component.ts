d152583c6f8377d23a423ed8f7e0fa881b7558f2
Ravi Kiran Ayinampudi <rxa80430@ucmo.edu>
2022-04-21 08:41:49 -0400

    Front end breweries list

--- ClientApp/src/app/brewerieslist/brewerieslist.component.ts
+++ ClientApp/src/app/brewerieslist/brewerieslist.component.ts
@@ -9,11 +9,43 @@ import { Router } from '@angular/router';
 })
 export class BrewerieslistComponent implements OnInit {
 
-  constructor() { }
+  public breweriesData: Brewery[];
+  baseUrl: string;
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
+    http.get<Brewery[]>(baseUrl + 'Breweries/GetAllBreweries').subscribe(result => {
+      this.breweriesData = result;
+      
+
+      //http.get<string[]>(baseUrl + 'Login').subscribe(result => {
+      //  console.log(result);
+      //}, error => console.error(error));
+
+    }, error => console.error(error));}
 
   ngOnInit(): void {
 
 
   }
 
+}
+
+interface Brewery {
+  id: string
+  name: string
+  brewery_Type: string;
+  street: string;
+  address_2: string;
+  address_3: string;
+  city: string;
+  state: string;
+  county_Province: string;
+  postal_Code: string;
+  country: string;
+  longitude: string;
+  latitude: string;
+  phone: string;
+  website_Url: string;
+  updated_At: string;
+  created_At: string;
 }
